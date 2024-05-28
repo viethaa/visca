413dbbd442fcf5ebe0fa518401f13ae6dd9a024c
KMinh <daukhacminh006@gmail.com>
2024-05-28 16:49:30 +0700

    classname

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer class='bg-slate-200 text-sm'>
-      <div class='text-center text-gray-600 py-4'>
-        <div class='container mx-auto'>
+    <footer className='bg-slate-200 text-sm'>
+      <div className='text-center text-gray-600 py-4'>
+        <div className='container mx-auto'>
           <p>
             &copy; 2024 Concordia International School Hanoi. All rights
             reserved.
           </p>
-          <div class='mt-2 gap-4'>
-            <p class='mt-2'>
+          <div className='mt-2 gap-4'>
+            <p className='mt-2'>
               Van Tri Golf Estate, Kim Nỗ, Đông Anh, Hà Nội | Phone: 024 3795
               8878 | Email: @concordiahanoi.org
             </p>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,15 +3,15 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header class='bg-slate-800 text-white font-sans p-4'>
-      <div class='flex items-center justify-end'>
-        <div class='flex gap-8'>
+    <header className='bg-slate-800 text-white font-sans p-4'>
+      <div className='flex items-center justify-end'>
+        <div className='flex gap-8'>
           <Link href='/'>Home</Link>
           <Link href='/contact'>Contact Directory</Link>
         </div>
       </div>
-      <div class='text-2xl md:text-4xl max-w-2xl mx-auto p-4 sm:p-8 text-center'>
-        <h1 class='font-bold'>
+      <div className='text-2xl md:text-4xl max-w-2xl mx-auto p-4 sm:p-8 text-center'>
+        <h1 className='font-bold'>
           Counselor Contacts of International Schools in Hanoi for University
           Visits
         </h1>
