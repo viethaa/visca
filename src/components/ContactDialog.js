9c399aa6c4f618fae795799dc584fc8bb073d887
KMinh <daukhacminh006@gmail.com>
2025-04-03 16:08:40 +0700

    forms 4

--- src/components/ContactDialog.js
+++ src/components/ContactDialog.js
@@ -11,6 +11,7 @@ import { Label } from './ui/label'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
+import ContactForm from './ContactForm'
 
 export default function ContactDialog() {
   return (
@@ -25,49 +26,7 @@ export default function ContactDialog() {
             Please fill out the form below to inquire about our website.
           </DialogDescription>
         </DialogHeader>
-        <form
-          id='contact-form'
-          name='contact form'
-          className='space-y-4'
-          netlify
-        >
-          <div>
-            <Label htmlFor='nameOrOrganization'>Name/Organization</Label>
-            <Input
-              type='text'
-              id='nameOrOrganization'
-              name='nameOrOrganization'
-              placeholder='Enter your name or organization'
-              required
-            />
-          </div>
-          <div>
-            <Label htmlFor='email'>Email</Label>
-            <Input
-              type='email'
-              id='email'
-              name='email'
-              placeholder='Enter your email'
-              required
-            />
-          </div>
-          <div>
-            <Label htmlFor='message'>Message</Label>
-            <Textarea
-              id='message'
-              name='message'
-              rows='4'
-              placeholder='Enter your message'
-              required
-            />
-          </div>
-          <Button
-            type='submit'
-            className='w-full bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-          >
-            Submit
-          </Button>
-        </form>
+        <ContactForm />
       </DialogContent>
     </Dialog>
   )
---
+++ src/components/ContactForm.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+
+export default function ContactForm() {
+  return (
+    <form
+      id='contact-form'
+      method='POST'
+      name='contact-form'
+      className='space-y-4'
+      data-netlify='true'
+    >
+      <div className='space-y-1'>
+        <Label htmlFor='nameOrOrganization'>Name/Organization</Label>
+        <Input
+          type='text'
+          id='nameOrOrganization'
+          name='nameOrOrganization'
+          placeholder='Enter your name or organization'
+          required
+        />
+      </div>
+      <div className='space-y-1'>
+        <Label htmlFor='email'>Email</Label>
+        <Input
+          type='email'
+          id='email'
+          name='email'
+          placeholder='Enter your email'
+          required
+        />
+      </div>
+      <div className='space-y-1'>
+        <Label htmlFor='message'>Message</Label>
+        <Textarea
+          id='message'
+          name='message'
+          rows='4'
+          placeholder='Enter your message'
+          required
+        />
+      </div>
+      <button
+        type='submit'
+        className='w-full bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+      >
+        Submit
+      </button>
+    </form>
+  )
+}
