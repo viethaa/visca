f2800e0c23a08aa85e03d50b52d7de54880889b4
viethaa <vietha.icloud@gmail.com>
2025-09-01 00:11:22 +0700

    Updated Assets

--- src/components/AdDialog.js
+++ src/components/AdDialog.js
@@ -1,34 +1,34 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-import { Label } from './ui/label'
-import { Input } from './ui/input'
-import { Textarea } from './ui/textarea'
-import { Button } from './ui/button'
-import ContactForm from './ContactForm'
+} from "@/components/ui/dialog";
 
 export default function AdDialog() {
-  const [open, setOpen] = React.useState(true)
+  const [open, setOpen] = React.useState(true);
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogDescription>
-            <img
-              src='/fair2025.png'
-              alt='Hanoi Fair 2025'
-              className='relative scale-95'
-            />
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent
+        className="
+          w-full sm:max-w-[500px] p-0
+          overflow-hidden rounded-2xl
+          border-[4px] border-gray-900/90
+          shadow-[0_20px_60px_rgba(0,0,0,0.9)]
+          [&>button]:bg-black [&>button]:text-gray-200
+          [&>button]:hover:bg-gray-950 [&>button]:hover:text-white
+          [&>button]:flex [&>button]:items-center [&>button]:justify-center
+          [&>button]:w-10 [&>button]:h-10 [&>button]:rounded-xl
+        "
+      >
+        <img
+          src="/fair2025.png"
+          alt="Hanoi Fair 2025"
+          className="block w-full h-full object-cover"
+        />
       </DialogContent>
     </Dialog>
-  )
+  );
 }
